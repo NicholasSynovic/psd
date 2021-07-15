ae3ba3657f2077610b911bcb233140b48c2a480c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-07-15 00:30:55 +0000

    Implement CLI selection and filtering

--- psd.py
+++ psd.py
@@ -1,14 +1,16 @@
 from requests import get, Response
 from bs4 import BeautifulSoup, ResultSet
 from bs4.element import Tag
+from requests.api import options
+from simple_term_menu import TerminalMenu
 
 
-def getFTPRoot() -> str:
-    html: Response = get(url="https://www.python.org/ftp/python/")
+def getHTML(url: str = "https://www.python.org/ftp/python/") -> str:
+    html: Response = get(url)
     return html.text
 
 
-def getLinks(soup: BeautifulSoup) -> dict:
+def getLinks(soup: BeautifulSoup, filter: str = "") -> dict:
     links: dict = {}
     tags: ResultSet = soup.find_all(name="a")
 
@@ -17,21 +19,30 @@ def getLinks(soup: BeautifulSoup) -> dict:
         key: str = tag.text.replace("/", "")
         value: str = "https://www.python.org/ftp/python/" + tag.get("href")
 
-        try:
-            int(key[0])
-            links[key] = value
-        except ValueError:
-            pass
+        if key.find(filter) != -1:
+            try:
+                int(key[0])
+                links[key] = value
+            except ValueError:
+                pass
 
     return links
 
 
-def getUserSelection(choices: list) -> str:
-    pass
+def getUserSelection(options: list) -> str:
+    terminal_menu = TerminalMenu(
+        options,
+        title="Python Version",
+        status_bar='Use "/" to search for a specific version. ENTER to select.',
+        status_bar_style=("fg_black", "bg_blue"),
+    )
+    menu_entry_index = terminal_menu.show()
+    print(options[menu_entry_index])
 
 
 if __name__ == "__main__":
-    site = getFTPRoot()
+    site = getHTML()
     soup: BeautifulSoup = BeautifulSoup(markup=site, features="html.parser")
     links: dict = getLinks(soup)
-    getUserSelection(list(links.keys()))
+    print(links.keys())
+    pythonVersion: str = getUserSelection(list(links.keys()))
